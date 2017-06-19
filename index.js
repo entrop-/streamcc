45520f785e5b9bd083692b35e2d1cfbec5ad0681
Tomek Strąk <entrop.tomek@gmail.com>
2017-06-19 16:28:29 +0200

    json export to csv

--- index.js
+++ index.js
@@ -5,12 +5,26 @@ var parse = require('JSONStream').parse,
     path = require('path'),
     zlib = require('zlib'),
     es = require('event-stream'),
+    jsonexport = require('jsonexport'),
 
     inStream = fs.createReadStream(path.join('input', 'data.ndjson.gz')),
     outStream = fs.createWriteStream(path.join('output', 'data.csv')),
 
     avarageFriends = 0,
     userCount = 0;
+
+let toCsv = (items) => {
+
+    console.log(items);
+    // const items = json3.items
+    // const replacer = (key, value) => value === null ? '' : value // specify how you want to handle null values here
+    // const header = Object.keys(items[0])
+    // let csv = items.map(row => header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','))
+    // csv.unshift(header.join(','))
+    // csv = csv.join('\r\n')
+    //
+     return items;
+}
 
 inStream
     .pipe(zlib.createGunzip())
@@ -22,17 +36,16 @@ inStream
         let age = new Date().getFullYear() - bdayYear;
         data.personal.age = age;
 
+        //collect friends data
         avarageFriends += data.friends.length;
         userCount ++;
 
-        // console.log(data);
-        // console.log('---------');
-        return [bdayYear, age,data.personal.age, data.personal.name].join(',') + '\n';
+        return JSON.stringify(data) + '\n';
 
     }))
+    .pipe(jsonexport())
     .pipe(outStream)
-
-        .on('finish', () => {
+    .on('finish', () => {
 
         console.log('average friends: ', avarageFriends / userCount  )
 
