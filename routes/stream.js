819c607d97a1f4de42029bf55300604360ae0749
Tomek Strąk <entrop.tomek@gmail.com>
2017-06-19 10:41:51 +0200

    remove old parts of code

--- routes/stream.js
+++ routes/stream.js
@@ -16,41 +16,6 @@ var chunk='';
 router.get('/', function(req, res, next) {
 
 
-    // var pipeline = fs.createReadStream('input/data.ndjson.gz')
-    //     .pipe(zlib.createGunzip())
-    //     .on('data', function (data){
-    //
-    //         chunk += data;
-    //         console.log('got it');
-    //         // var parser = JSONStream.parse(chunk);
-    //         //
-    //         //
-    //         //
-    //         // parser.on('data', function(unzipped) {
-    //         //     console.log('received:', unzipped.toString());
-    //         // });
-    //         data.toString();
-    //
-    //     })
-    //     // .on('end',function(){
-    //     //
-    //     // })
-    //     // .pipe(process.stdout)
-    //     // .pipe(parser)
-    //     .pipe(fs.createWriteStream('output/data.ndjson'))
-    //     .on('error', function(e) {console.log(e) });
-
-    // var lineReader = readline.createInterface({
-    //     input: fs.createReadStream('input/data.ndjson.gz').pipe(zlib.createGunzip()),
-    //     output: fs.createWriteStream('output/data.csv'),
-    //     terminal: false
-    // });
-    //
-    // lineReader.on('line', function (line) {
-    //     console.log('Line from file:', line);
-    // });
-
-    //(path.join('input', 'file.json'))
     var s = fs.createReadStream(path.join('input', 'data.ndjson.gz'))
             .pipe(zlib.createGunzip())
             .pipe(es.split())
@@ -70,6 +35,7 @@ router.get('/', function(req, res, next) {
                         console.log('Read entire file.')
                     })
             )
+            .pipe(parser("*"))
             .pipe(fs.createWriteStream(path.join('output', 'data.csv')))
         ;
 
